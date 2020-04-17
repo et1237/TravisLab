a6dca84b4ea383cc470095ebd43492e03d1a5c2e
Ethan <ethan.tucker99@gmail.com>
2020-04-17 12:00:02 -0700

    removed errant semicolon

--- src/temp/HelloWorld.java
+++ src/temp/HelloWorld.java
@@ -1,7 +1,7 @@
 package temp;
 import java.util.logging.Logger;
 public class HelloWorld {
-    public static final Logger LOGGER = Logger.getLogger(HelloWorld.class.getName());;
+    public static final Logger LOGGER = Logger.getLogger(HelloWorld.class.getName());
 
 	public static void main(String[] args) {
 		LOGGER.info("HelloWorld");
